d1399db568f0045130fd9d82c25547971065c799
Buooy <aaron.lee@buooy.com>
2015-07-24 11:16:58 +0800

    Added "Namespace"

--- v0.6.5/init.php
+++ v0.6.5/init.php
@@ -10,7 +10,7 @@ if ( ! defined( 'WPINC' ) ) {
 if( is_admin() ){
 	
 	// Checks that the user is on localhost. Aviary does not work on localhost
-	is_on_localhost();
+	buooy_aviary_is_on_localhost();
 		
 	include_once __DIR__.'/admin/class-buooy-aviary-editor-admin.php';
 	include_once __DIR__.'/admin/class-buooy-aviary-editor.php';
@@ -18,13 +18,13 @@ if( is_admin() ){
 }
 
 // Checks if the user is on localhost
-function is_on_localhost(){
+function buooy_aviary_is_on_localhost(){
 	
 	$whitelist = array(	'127.0.0.1','::1' );
 	if(in_array($_SERVER['REMOTE_ADDR'], $whitelist) ){
 		
 		// Outputs the warning banner
-		add_action( 'admin_footer', 'aviary_localhost_warning' );
+		add_action( 'admin_footer', 'buooy_aviary_localhost_warning' );
 		return true;
 	}
 	return false;
@@ -32,7 +32,7 @@ function is_on_localhost(){
 }
 
 // Displays the localhost warning that says you cannot use it without an online server
-function aviary_localhost_warning(){
+function buooy_aviary_localhost_warning(){
 	
 	$banner = 	'<div id="warning-banner" style="width: 100%; color: white; position:fixed; bottom: 0; z-index:9999; background-color: #c0392b;">';
 	$banner .=		'<p style="line-height: 50px; padding-left: 20px; padding-right: 20px;">';
